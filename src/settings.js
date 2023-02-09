c5040ce2de247818352755a4947295e1dc39a892
dfiialko <denys.fiialko@gmail.com>
2023-02-08 16:37:33 -0800

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,3 @@
-// export const GOOGLE_API_KEY = 'AIzaSyD7jKf5q3d9q4l4fj5g6h7i8j9k0l1m2n3o';
-// export const RENTFASTER_API_URL = 'https://www.rentfaster.ca/ab/edmonton/rentals/?l=14,53.4657,-113.4556&beds=3&beds=3%20%2B%20Den&baths=1.5&baths=1&baths=2';
-// export const CITY_DEFAULT = 'Edmonton';
-
 export const settings = {
     googleApiKey: '',
     city: 'Edmonton',
